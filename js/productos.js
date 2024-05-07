7a2082427f9b68a8f52c00bd0f4c50d2fa52f662
unknown <micaoggero17@gmail.com>
2024-05-07 12:10:15 -0300

    PreEntrega3

---
+++ js/productos.js
@@ -0,0 +1,82 @@
+const listaProductos = [
+    {
+        id:1,
+        nombre: "Conjunto de encaje",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+
+    {
+        id:2,
+        nombre: "Conjunto de algodón",
+        precio: 5500,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+    
+    {
+        id:3,
+        nombre: "Conjunto deportivo",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+
+    {
+        id:4,
+        nombre: "Conjunto de encaje",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+
+    {
+        id:5,
+        nombre: "Conjunto de encaje",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+
+    {
+        id:6,
+        nombre: "Conjunto de encaje",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+
+    {
+        id:7,
+        nombre: "Medias",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    },
+
+    {
+        id:8,
+        nombre: "Bombis",
+        precio: 3962,
+        codigo: "AVR1004",
+        color: ["rojo", "blanco", "negro"],
+        talle: [85, 90, 95, 100],
+        imagen: "imagenes/conjunto.jpg",
+    }
+]
+
